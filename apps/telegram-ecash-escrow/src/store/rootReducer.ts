966057bd6a86bb66c5b4cc286e3aa5f1e0177d95
vince8x <vince.tran8x@gmail.com>
2024-05-29 10:46:48 +0700

    feat: add redux-persist

--- apps/telegram-ecash-escrow/src/store/rootReducer.ts
+++ apps/telegram-ecash-escrow/src/store/rootReducer.ts
@@ -1,11 +1,59 @@
 import { counterReducer } from '@bcpros/counter';
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import indexedDbStorage from 'redux-persist-indexeddb-storage';
+
+const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: number) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? indexedDbStorage('escrow-indexeddb')
+    : createNoopStorage();
 
 
+// const _persistConfig = {
+//   timeout: 1000,
+//   key: 'root',
+//   version: 0,
+//   storage: storage('escrow-indexeddb'),
+//   blacklist: [
+//     'accounts',
+//     'router',
+//     'modal',
+//     'action-sheet',
+//     'toast',
+//     'wallet',
+//     'api',
+//     'root',
+//     'posts',
+//     'pages',
+//     'burn',
+//     'loading',
+//     'notifications'
+//   ],
+// };
 
-export const clientReducer = combineReducers({
-  counter: counterReducer,
+const counterPersistConfig = {
+  key: 'counter',
+  storage,
+  whitelist: ['counter']
+};
+
+const reducer = combineReducers({
+  counter: persistReducer(counterPersistConfig, counterReducer)
 });
 
-export default clientReducer;
+export default reducer;
 
