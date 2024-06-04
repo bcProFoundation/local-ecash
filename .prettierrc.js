c31b9a1369e8ea14879e7b4fefb1eba5784e5cd1
Ken Nguyen <thekha2710@gmail.com>
2024-06-04 11:08:27 +0700

    fix: stylex

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 120,
+  "proseWrap": "always",
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "none",
+  "plugins": [
+    "prettier-plugin-stylex-key-sort",
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-packagejson"
+  ]
+}
