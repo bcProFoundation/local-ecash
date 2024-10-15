4ab81ae14973657ac63fb4c41dd6ad865a1a5e07
kennguyendev <74177525+KenNguyen-dev@users.noreply.github.com>
2024-10-15 12:32:11 +0700

    fix: #65 (#71)

--- apps/telegram-ecash-escrow/pages/api/auth/[...nextauth].ts
+++ apps/telegram-ecash-escrow/pages/api/auth/[...nextauth].ts
@@ -83,6 +83,34 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
     maxAge: 15552000 // 6 months
+  },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    }
   }
 };
 
