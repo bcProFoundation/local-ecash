8509f34dd788b8a0cb3d0be098da0e5a5c209617
kennguyendev <74177525+KenNguyen-dev@users.noreply.github.com>
2024-12-20 11:54:26 +0700

    feat: issue #159 (#164)

--- apps/telegram-ecash-escrow/src/components/Header/Header.tsx
+++ apps/telegram-ecash-escrow/src/components/Header/Header.tsx
@@ -78,6 +78,24 @@ const StyledAvatar = styled('div')(({ theme }) => ({
   }
 }));
 
+const StyledAvatar = styled.div`
+  width: 1.55em; /* Adjust size as needed */
+  height: 1.55em;
+  border-radius: 50%;
+  overflow: hidden;
+  display: inline-block;
+  border: 2px solid #0076c4; /* Optional: Add a border */
+  background-image: url('./eCash.svg'); /* Optional: Placeholder image */
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
 export default function Header() {
   const router = useRouter();
   const { data, status } = useSession();
@@ -140,7 +158,15 @@ export default function Header() {
       </div>
       <div className="wallet-minimals">
         <IconButton onClick={e => handlePopoverOpen(e)}>
-          <AccountCircleRoundedIcon fontSize="large" />
+          {data && data.user.image ? (
+            <StyledAvatar>
+              <picture>
+                <img src={data.user.image} alt="" />
+              </picture>
+            </StyledAvatar>
+          ) : (
+            <AccountCircleRoundedIcon fontSize="large" />
+          )}
         </IconButton>
         <Popover
           id="mouse-over-popover"
